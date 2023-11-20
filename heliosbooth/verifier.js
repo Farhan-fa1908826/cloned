31c4611c44ff714f4683ba6d89240ae12d6d722b
Hosam Zarouk <hz1902702@qu.edu.qa>
2023-11-20 22:40:54 +0300

    hh

--- heliosbooth/verifier.js
+++ heliosbooth/verifier.js
@@ -6,25 +6,30 @@ function verify_ballot(election_raw_json, encrypted_vote_json, status_cb) {
     try {
 	election = HELIOS.Election.fromJSONString(election_raw_json);
 	var election_hash = election.get_hash();
-	status_cb("election fingerprint is " + election_hash);
+	// status_cb("election fingerprint is " + election_hash);
 	
 	// display ballot fingerprint
 	encrypted_vote = HELIOS.EncryptedVote.fromJSONObject(encrypted_vote_json, election);
 	status_cb("ballot tracker is " + encrypted_vote.get_hash());
 	
       // check the hash
-      if (election_hash == encrypted_vote.election_hash) {
-          status_cb("election fingerprint matches ballot");
-      } else {
-          overall_result = false;
-          status_cb("PROBLEM = election fingerprint does not match");          
-      }
+    //   if (election_hash == encrypted_vote.election_hash) {
+    //     //   status_cb("election fingerprint matches ballot");
+    //   } else {
+    //       overall_result = false;
+    //     //   status_cb("PROBLEM = election fingerprint does not match");          
+    //   }
+
+          // check the hash
+    if (!(election_hash == encrypted_vote.election_hash)) {
+    overall_result = false;        
+    }
       
       // display the ballot as it is claimed to be
       status_cb("Ballot Contents:");
       _(election.questions).each(function(q, qnum) {
 	      if (q.tally_type != "homomorphic") {
-		  status_cb("WARNING: the tally type for this question is not homomorphic. Verification may fail because this verifier is only set up to handle homomorphic ballots.");
+		//   status_cb("WARNING: the tally type for this question is not homomorphic. Verification may fail because this verifier is only set up to handle homomorphic ballots.");
 	      }
         
 	      var answer_pretty_list = _(encrypted_vote.encrypted_answers[qnum].answer).map(function(aindex, anum) {
@@ -33,22 +38,33 @@ function verify_ballot(election_raw_json, encrypted_vote_json, status_cb) {
 	      status_cb("Question #" + (qnum+1) + " - " + q.short_name + " : " + answer_pretty_list.join(", "));
       });
       
-      // verify the encryption
-      if (encrypted_vote.verifyEncryption(election.questions, election.public_key)) {
-          status_cb("Encryption Verified");
-      } else {
-          overall_result = false;
-          status_cb("PROBLEM = Encryption doesn't match.");
+    //   // verify the encryption
+    //   if (encrypted_vote.verifyEncryption(election.questions, election.public_key)) {
+    //     //   status_cb("Encryption Verified");
+    //   } else {
+    //       overall_result = false;
+    //     //   status_cb("PROBLEM = Encryption doesn't match.");
+    //   }
+
+        // verify the encryption
+        if (!(encrypted_vote.verifyEncryption(election.questions, election.public_key))) {
+        overall_result = false;
       }
       
-      // verify the proofs
-      if (encrypted_vote.verifyProofs(election.public_key, function(ea_num, choice_num, result) {
-      })) {
-          status_cb("Proofs ok.");
-      } else {
-          overall_result = false;
-          status_cb("PROBLEM = Proofs don't work.");
-      }
+    //   if (encrypted_vote.verifyProofs(election.public_key, function(ea_num, choice_num, result) {
+    //   })) {
+    //     //   status_cb("Proofs ok."); 
+    //   } else {
+    //       overall_result = false;
+    //     //   status_cb("PROBLEM = Proofs don't work.");
+    //   }
+
+    // verify the proofs
+    if (!(encrypted_vote.verifyProofs(election.public_key, function(ea_num, choice_num, result) {
+    }))) {
+        overall_result = false;
+    }
+
     } catch (e) {
       status_cb('problem parsing election or ballot data structures, malformed inputs: ' + e.toString());
       overall_result=false;
